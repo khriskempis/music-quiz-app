f00ef2c300b0567846a0fe9d609c091ba581eb85
Khris Kempis <khriskempis@gmail.com>
2018-11-27 23:52:17 -0800

    updated base api route

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL =
-    // process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
-    'https://young-crag-97462.herokuapp.com/api';
+    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/api';
+    // 'https://young-crag-97462.herokuapp.com/api';
