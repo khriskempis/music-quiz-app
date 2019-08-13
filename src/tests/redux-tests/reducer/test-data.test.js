53eb4fbfa2c472d1cba5595d2f7265f2686f45a4
Khris Kempis <khriskempis@gmail.com>
2019-08-13 14:35:37 -0700

    update tests, revisit App smoke test

--- src/tests/redux-tests/reducer/test-data.test.js
+++ src/tests/redux-tests/reducer/test-data.test.js
@@ -1,15 +1,14 @@
-import testDataReducer from '../../../reducers/test-data';
-import * as _ from '../../../actions/test-data';
+import testDataReducer from "../../../reducers/test-data";
+import * as _ from "../../../actions/test-data";
 
-describe('testDataReducer', ()=> {
+describe("testDataReducer", () => {
   const initialState = {
     loading: false,
     error: null,
-  
+
     numberOfQuestions: 10,
     currentQuestion: 1,
-    currentCard: {
-    },
+    currentCard: {},
     testName: "Treble Clef",
     wrongAnswers: [],
     correctAnswers: [],
@@ -18,36 +17,30 @@ describe('testDataReducer', ()=> {
     timeRemaining: null,
     outOfTime: false,
     testType: "practice",
-    responses: [
-      "Great Job!",
-      "Keep It Up!",
-      "Amazing!",
-      "Are you a Musician?"
-    ],
+    responses: ["Great Job!", "Keep It Up!", "Amazing!", "Are you a Musician?"],
     data: [
       {
-        imgUrl: "https://res.cloudinary.com/khrisraymond/image/upload/v1543184876/bass/treble%20jpgs/C4.jpg",
+        imgUrl: "../../assets/BASS/JPGs/C4.jpg",
         noteId: "C4",
         note: "C",
         clef: "treble"
       },
       {
-        imgUrl: "https://res.cloudinary.com/khrisraymond/image/upload/v1543184876/bass/treble%20jpgs/D4.jpg",
+        imgUrl: "../../assets/BASS/JPGs/D4.jpg",
         noteId: "D4",
         note: "D",
         clef: "treble"
       }
     ]
-  }
+  };
 
-  it('should set initial state when nothing is passed in', ()=> {
-    const state = testDataReducer(undefined, {type: '__UNKOWN'});
+  it("should set initial state when nothing is passed in", () => {
+    const state = testDataReducer(undefined, { type: "__UNKOWN" });
     expect(state).toEqual(initialState);
-  })
-  it('should return currect state on unkown action', ()=> {
+  });
+  it("should return currect state on unkown action", () => {
     const currentState = {};
-    const state = testDataReducer(currentState, {type: '__UNKNOWN'});
+    const state = testDataReducer(currentState, { type: "__UNKNOWN" });
     expect(state).toBe(currentState);
-  })
-
-})
+  });
+});
